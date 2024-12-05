18352eb8e971ccb797c1648ce18c4192d5e1fa3a
Luis Chavez <lchavezpozo@gmail.com>
2024-12-04 21:28:24 -0500

    first commit

---
+++ PokedexApp/Features/PokemonList/Presentation/PokemonDetail/PokemonDetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PokemonDetailViewController.swift
+//  PokedexApp
+//
+//  Created by Luis Chavez pozo on 4/12/24.
+//
+
+
+import UIKit
+
+class PokemonDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+}
---
+++ PokedexApp/Features/PokemonList/Presentation/PokemonList/PokemonListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonListViewController.swift
+//  PokedexApp
+//
+//  Created by Luis Chavez pozo on 4/12/24.
+//
+
+import UIKit
+
+class PokemonListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+}
